94af5aabf55dd5320600b63aea6179acb2a0ae5a
jacome <diego.jacome.cc@gmail.com>
2022-09-08 21:23:46 -0300

    multiplica

--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import "fmt"
 
 func main() {
 	fmt.Println(soma(1, 2, 3))
+	fmt.Println(multiplica(1, 2, 3))
 }
 
 func soma(i ...int) int {
 	total := 0
 	for _, v := range i {
 		total = total + v
+	}
+	return total
+}
+
+func multiplica(i ...int) int {
+	total := 1
+	for _, v := range i {
+		total = total * v
 	}
 	return total
 }
